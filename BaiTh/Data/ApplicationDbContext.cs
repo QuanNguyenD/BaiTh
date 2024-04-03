6a23dcf7421be8084a53e6803de49edec881d322
Quan <nguyenquocanhquangl@gmail.com>
2024-04-03 23:15:00 +0700

    Add project files.

---
+++ BaiTh/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaiTh.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiTh.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
